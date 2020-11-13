58e14b8cc85991fc11f72b9dfa7b8d6d636d35ef
Shariq Jamil <shariq1989@gmail.com>
2020-11-12 17:36:56 -0700

    Finish upload (#6)
    * added Meteor-Files client and server file upload code
    
    * configure upload code
    
    * upload images working locally
    
    * remove logging

--- imports/ui/components/Upload.vue
+++ imports/ui/components/Upload.vue
@@ -1,42 +1,92 @@
 <template>
   <div>
-    <button @click="increment">Click Me</button>
-    <p>Upload receipts</p>
+    <!-- Upload receipt form -->
+    <div v-if="!image">
+      <h2>Select an image</h2>
+      <input type="file" @change="onFileChange">
+    </div>
+    <!-- Displayed after upload complete -->
+    <div v-else>
+      <img :src="image" alt="uploaded image"/>
+      <button @click="uploadAnotherReceipt">Upload another</button>
+    </div>
   </div>
 </template>
 
 <script>
 import {Receipts} from "../../api/collections/Receipts";
+
 export default {
   data() {
     return {
       counter: 0,
-      uploadedImage: null
+      image: null
     }
   },
   methods: {
-    increment() {
-      this.counter += 1
+    onFileChange(e) {
+      let files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      if (e.currentTarget.files && e.currentTarget.files[0]) {
+        let file = e.currentTarget.files[0];
+        this.uploadReceipt(file)
+      }
+      this.displayImage(files[0]);
+    },
+
+    displayImage(file) {
+      const reader = new FileReader();
+      const vm = this;
+
+      reader.onload = (e) => {
+        vm.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    uploadAnotherReceipt: function () {
+      this.image = null;
+    },
+    uploadReceipt(image) {
+      let upload = Receipts.insert({
+        file: image,
+        streams: 'dynamic',
+        chunkSize: 'dynamic',
+      }, false);
+      upload.on('start', function () {
+        //TODO: Sets upload progress
+        //template.currentUpload.set(this);
+      });
+
+      upload.on('end', function (error, fileObj) {
+        if (error) {
+          alert(`Error during upload: ${error}`);
+        } else {
+          alert(`File "${fileObj.name}" successfully uploaded`);
+        }
+        // removes upload progress
+        //template.currentUpload.set(false);
+      });
+      upload.start();
+      // Clear form
+      this.image = null;
     }
   },
-  uploadReceipt() {
-    Receipts.insert({
-      image: this.uploadedImage,
-      uploadTimestamp: new Date()
-    });
-    // Clear form
-    this.uploadedImage = null;
-  }
-
-
 }
 </script>
 
 <style scoped>
+img {
+  width: 30%;
+  display: block;
+  margin: auto auto 10px;
+}
+
 p {
   font-family: serif;
 }
-  p {
-    font-family: serif;
-  }
+
+p {
+  font-family: serif;
+}
 </style>
