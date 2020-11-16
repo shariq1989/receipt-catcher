8e300cc0b25e19d904759611a781b55171559256
Shariq Jamil <shariq1989@gmail.com>
2020-11-15 21:24:10 -0700

    Updated README for part 2 and added vue testing lib (#8)
    * update project part 2 readme
    
    * update readme for part 2

--- tests/main.js
+++ tests/main.js
@@ -1,20 +1,31 @@
 import assert from "assert";
+import { shallowMount } from '@vue/test-utils'
+
 
 describe("receipt-catcher", function () {
-  it("package.json has correct name", async function () {
-    const { name } = await import("../package.json");
-    assert.strictEqual(name, "receipt-catcher");
-  });
-
-  if (Meteor.isClient) {
-    it("client is not server", function () {
-      assert.strictEqual(Meteor.isServer, false);
+    it("package.json has correct name", async function () {
+        const {name} = await import("../package.json");
+        assert.strictEqual(name, "receipt-catcher");
     });
-  }
 
-  if (Meteor.isServer) {
-    it("server is not client", function () {
-      assert.strictEqual(Meteor.isClient, false);
+    if (Meteor.isClient) {
+        it("client is not server", function () {
+            assert.strictEqual(Meteor.isServer, false);
+        });
+    }
+
+    if (Meteor.isServer) {
+        it("server is not client", function () {
+            assert.strictEqual(Meteor.isClient, false);
+        });
+    }
+    it("package.json has correct name", async function () {
+        const {name} = await import("../package.json");
+        assert.strictEqual(name, "receipt-catcher");
     });
-  }
+    it("package.json has correct name", async function () {
+        const {name} = await import("../package.json");
+        assert.strictEqual(name, "receipt-catcher");
+    });
+
 });
