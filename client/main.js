418ba7a7e4393cb823e203528194cbb134ba6c87
remioldham <remioldham@gmail.com>
2020-12-05 14:51:17 -0500

    added routing to client code

--- client/main.js
+++ client/main.js
@@ -1,14 +1,39 @@
-import Vue from 'vue'
+import {Meteor} from 'meteor/meteor';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 import VueMeteorTracker from 'vue-meteor-tracker';
-import '../imports/ui/plugins'
-
+import '../imports/ui/plugins';
+import App from '../imports/ui/App.vue'
+Vue.use(VueRouter);
 Vue.use(VueMeteorTracker);
 
-import App from '../imports/ui/App.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: App,
+  },
+  {
+    path: '*',
+    name: 'notfound',
+    component: ()=>import('/imports/ui/components/NotFound.vue')
+  },
+  {
+    path: '/receiptId',
+    name: 'receiptDetails',
+    component: ()=>import('/imports/ui/components/Receipt.vue'),
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes,
+});
 
 Meteor.startup(() => {
     new Vue({
         el: '#app',
         ...App,
+        router: router,
     })
 })
