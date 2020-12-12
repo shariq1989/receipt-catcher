fb2b67ee8a6f8a3d98518eb3ef391022e8a1a1e9
remioldham <remioldham@gmail.com>
2020-12-12 18:25:22 -0500

    Updates to ReceiptDetails.vue
    Added some content to this page. Issues with getting data from ostrio, find().fetch() seems to be returning undefined.

--- imports/ui/components/ReceiptDetails.vue
+++ imports/ui/components/ReceiptDetails.vue
@@ -1,9 +1,41 @@
 <template>
   <div>
+
     <h2>Details</h2>
-    <div>
-      <img src=receipt.file></img>
+    <div id='metadata'>
+      <p>Store Name:</p>
+      <p>Date:</p>
+      <p>Category:</p>
+      <p>Total Spent:</p>
+      <img src=SomeImagePathHere></img> <!-- NEEDS A WORKING IMAGE PATH -->
+
+      <!-- <el-button>Edit</el-button> -->
     </div>
     <a href="/receipts">Return to Table</a>
   </div>
 </template>
+
+<script>
+import {Receipts} from "../../api/collections/Receipts";
+import Receipt from './Receipt.vue';
+
+export default {
+  components: {
+    Receipt
+  },
+  data() {
+    return {
+      receipts: null,
+    }
+  },
+  methods: {
+
+  },
+  meteor: {
+    getReceipt(){
+      let receiptId = window.location.pathname.split('/')[2];
+      this.receipts = Receipts.find({_id: receiptId}).fetch();
+    }
+  }
+}
+</script>
